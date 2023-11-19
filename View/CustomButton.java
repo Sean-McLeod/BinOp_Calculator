02b1a17f88521ab2e057ddd88499f042a5ee045d
FariaNK <nawed.khan7904@gmail.com>
2023-11-18 23:47:00 -0500

    [DEV-2] Added CustomButton abstract class

---
+++ View/CustomButton.java
@@ -0,0 +1,42 @@
+package View;
+
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public abstract class CustomButton extends Button {
+
+    public CustomButton(String text) {
+        super(text);
+        setDefaultStyle();
+        highlightButton();
+    }
+
+    public void setDefaultStyle(){
+        setPrefWidth(60);
+        setPrefHeight(60);
+        setTextFill(Color.WHITE);
+        setFont(Font.font(15));
+        setStyle("-fx-background-color: green;");    }
+
+    public void highlightButton(){
+        setOnMousePressed(event ->
+                setStyle("-fx-background-color: orange;"));
+        setOnMouseReleased(event ->
+                setStyle("-fx-background-color: green;"));
+        setOnMouseEntered(event -> {
+            setOpacity(0.8);
+            setStyle("-fx-border-color: orange; -fx-border-width: 2px; -fx-background-color: green;");});
+        setOnMouseExited(event -> {
+            setOpacity(1.0);
+            setStyle("-fx-background-color: green;");});
+    }
+
+    public void increaseFontSize(){
+        double newSize = getFont().getSize() + 5;
+        setFont(new Font(newSize));
+    }
+
+    public abstract void setUpEventHandler();
+
+}
