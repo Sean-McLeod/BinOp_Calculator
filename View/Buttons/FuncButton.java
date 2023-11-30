a617c7692863d84d9ee1fb506b8e603388e59bc7
FariaNK <nawed.khan7904@gmail.com>
2023-11-30 01:17:33 -0500

    [DEV 4.7] Implemented change font size feature

---
+++ View/Buttons/FuncButton.java
@@ -0,0 +1,8 @@
+package View.Buttons;
+
+public class FuncButton extends CustomButton{
+    public FuncButton(String text) {
+        super(text);
+    }
+
+}
---
+++ View/InputLabel.java
@@ -0,0 +1,39 @@
+package View;
+
+import javafx.scene.control.Label;
+
+public class InputLabel extends Label {
+    public int fontSize; //font size of label text
+    public String fontColour; //font colour of label text
+    public String backColour; //background colour of label
+
+    public InputLabel(){
+        super();
+        setLabelStyle("#393e46", 15, "white");
+    }
+    public InputLabel(String text){
+        super(text);
+        setLabelStyle("#393e46", 15, "white");
+    }
+
+    /**
+     * set font size, font colour, and background colour of the input label
+     */
+    public void setLabelStyle(String backColour, int fontSize, String fontColour){
+        this.fontColour = fontColour;
+        this.fontSize = fontSize;
+        this.backColour = backColour;
+        setPrefSize(340, 70);
+        setStyle("-fx-font-family: 'Arial'; -fx-font-size:"+fontSize+"; -fx-alignment: center-right; -fx-padding: 15px;" +
+                "-fx-background-color:"+backColour+"; -fx-background-radius: 10px; -fx-text-fill:"+fontColour+";");
+    }
+
+    /**
+     * increase font size of the label text in increments of 5
+     */
+    public void increaseFontSize(){
+        fontSize = (fontSize < 25) ? fontSize + 5 : 15;
+        setStyle("-fx-font-family: 'Arial'; -fx-font-size:"+fontSize+"; -fx-alignment: center-right; -fx-padding: 15px;" +
+                "-fx-background-color:"+backColour+"; -fx-background-radius: 10px; -fx-text-fill:"+fontColour+";");
+    }
+}
