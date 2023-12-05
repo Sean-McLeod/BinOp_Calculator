8bd24da8a41a0a1fdfc562f79b39d39b1ab053f0
Sean-Mcleod <Sean.mcleod@mths.ca>
2023-12-05 12:53:55 -0500

    Add documentation for classes BinOp, MemoryDLL, IExpression, and Value

--- Model/Expressions/BinOp.java
+++ Model/Expressions/BinOp.java
@@ -1,33 +1,56 @@
 package Model.Expressions;
 
-
+/**
+ * Class BinOp
+ * Implements the IExpression Interface
+ * This class represents the binary operation between two expressions,
+ * in a binary tree-like structure
+ */
 public class BinOp implements IExpression {
     protected IExpression leftExpression;
     protected IExpression rightExpression;
     private String operation;
 
-
+    /**
+     * BinOp constructor
+     * Initializes attributes
+     */
     public BinOp(String op) {
         this.leftExpression = null;
         this.operation = op;
         this.rightExpression = null;
     }
 
+    /**
+     * Alternative BinOp constructor
+     * Initializes attributes
+     */
     public BinOp(IExpression left, String op, IExpression right) {
         this.leftExpression = left;
         this.operation = op;
         this.rightExpression = right;
     }
 
+    /**
+     * Checks if the current BinOp has leaves
+     * returns true when there are no leaves
+     */
     public boolean hasLeafs() {
         return (this.rightExpression == null && this.leftExpression == null);
     }
 
+    /**
+     * returns the String version of the result of the binary operation
+     */
     @Override
     public String toString() {
         if (this.hasLeafs()) {return operation;} else {return "" + this.Evaluate();}
     }
 
+    /**
+     * Evaluates the binary operation
+     * operations include: + (addition), - (subtraction), * (multiplication), / (division), ^ (power)
+     */
     @Override
     public Double Evaluate() {
         if (operation.equals("+")) {
@@ -48,18 +71,32 @@ public class BinOp implements IExpression {
         return null;
     }
 
+    /**
+     * returns the String version of the binary operation
+     */
     @Override
     public String StringRepresentation() {
         return "(" + leftExpression.StringRepresentation() + operation + rightExpression.StringRepresentation() + ")";
     }
 
+    /**
+     * Addition
+     */
     private Double Add(double left, double right) {
         return left + right;
     }
+
+    /**
+     * Subtraction
+     */
     private Double Subtract(double left, double right) {
         return left - right;
     }
 
+    /**
+     * Division
+     * Catches Exception with invalid input
+     */
     private Double Divide(Double left, Double right) {
         try {
             return left / right;
@@ -69,6 +106,10 @@ public class BinOp implements IExpression {
         }
     }
 
+    /**
+     * Multiplication
+     * Catches Exception with invalid input
+     */
     private Double Multiply(double left, double right) {
         try {
             return left * right;
@@ -78,6 +119,10 @@ public class BinOp implements IExpression {
         }
     }
 
+    /**
+     * Power
+     * Catches Exception with invalid input
+     */
     private Double Power(double left, double right) {
         try {
             return Math.pow(left, right);
